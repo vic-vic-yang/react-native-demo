e967c8c35f95a5488c42a6b1462bd123f513dd5c
yangshaobo <120237541@qq.com>
2019-05-16 18:09:21 +0800

    增加地图

--- src/pages/demo/map.js
+++ src/pages/demo/map.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import {FlatList,View,Text,TouchableOpacity} from "react-native"
-import {Actions} from "react-native-router-flux";
-
-
+import { FlatList, View, Text, TouchableOpacity ,StyleSheet} from "react-native"
+import { Actions } from "react-native-router-flux";
+import { MapView, MapTypes, Geolocation, Overlay } from 'react-native-baidu-map'
+const { Marker, Arc, Circle, Polyline, Polygon, InfoWindow } = Overlay
 export default class map extends Component {
 
 
-    render(){
-        return(
-           <View>
-               <Text>map</Text>
-           </View>
+    render() {
+        return (
+            <View style={styles.contain}> 
+                <MapView style={{height:400}}>
+
+                </MapView>
+            </View>
+
         )
     }
 }
+const styles = StyleSheet.create({
+    contain: {
+       flex:1,
+       backgroundColor:"#fff"
+
+    }
+})
